af6f3f274a5b8598d792031abb9f3877ecc97977
Oleg Solomka <legomushroom@gmail.com>
2022-05-18 22:50:25 +0000

    add vscode launch config

--- f3discovery/src/11-usart/src/main.rs
+++ f3discovery/src/11-usart/src/main.rs
@@ -8,10 +8,16 @@ use aux11::{entry, iprint, iprintln};
 fn main() -> ! {
     let (usart1, _mono_timer, _itm) = aux11::init();
 
-    // Send a single character
-    usart1
-        .tdr
-        .write(|w| w.tdr().bits(u16::from(b'X')) );
+    let str = "\r\nThe quick brown fox jumps over the lazy dog.";
+
+    for c in str.chars() {
+        while usart1.isr.read().txe().bit_is_clear() {}
+
+        // Send a single character
+        usart1
+            .tdr
+            .write(|w| w.tdr().bits(c as u16));
+    }
 
     loop {}
 }
