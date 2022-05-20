ef925c9c383f47379c52e50cb9355c30d56da03b
Oleg Solomka <legomushroom@gmail.com>
2022-05-20 06:53:00 +0000

    amend

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,14 @@ use stm32f3_discovery::stm32f3xx_hal::prelude::*;
 #[allow(unused_imports)]
 use cortex_m_rt::entry;
 
+// use stm32f3_discovery::stm32f3xx_hal::pac::usart1;
+// use stm32f3_discovery::stm32f3xx_hal::pac::USART1;
+
 #[entry]
 fn main() -> ! {
-    // let (usart1, _mono_timer, _itm) = aux11::init();
+    // let usart1: &'static mut usart1::RegisterBlock = unsafe {
+    //     &mut *(USART1::ptr() as *mut _)
+    // };
 
     // let str = "\r\nThe quick brown fox jumps over the lazy dog.";
 
